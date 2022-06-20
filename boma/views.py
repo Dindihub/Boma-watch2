a6df40ddd2affaa5e9bb628d7ef498df5cf35c52
Dindihub <sadindi03@gmail.com>
2022-06-20 15:06:45 +0300

    Code-add search,addbusinesses annd post views'

--- boma/views.py
+++ boma/views.py
@@ -115,12 +115,97 @@ def create_hood(request):
 
 def join_hood(request, id):
     neighbourhood=NeighbourHood.objects.get(id=id)
+    posts=Post.objects.filter(hood=neighbourhood)
+    business=Business.objects.filter(neighbourhood=neighbourhood)
     request.user.profile.neighbourhood = neighbourhood
     request.user.profile.save()
 
     context={
             'neighbourhood' : neighbourhood,
+            'posts':posts,
+            'business':business,
             
             }
-    return render(request,'join_hood.html',context ) 
+    return render(request,'join_hood.html',context )
+
+# @login_required(login_url='login')
+# def update_hood(request):
+#     neighbourHood = NeighbourHood.objects.get(user=request.profile.user)
+
+#     if request.method == 'POST':
+#         hood_form = UpdateHoodForm(request.POST, request.FILES, instance=request.user.profile)
+#         if  hood_form.is_valid():
+#             hood_form.save()
+#             return redirect('join_hood')
+       
+#     else:
+        
+#         hood_form = UpdateHoodForm(instance=request.user.profile)
+             
+#     context={
+#         'neighbourhood':neighbourHood,
+      
+#         'hood_form': hood_form,
+        
+#         }
+    
+#     return render(request, 'update_hood.html',context)
+ 
+@login_required(login_url='login')
+def create_post(request, hood_id):
+    hood = NeighbourHood.objects.get(id=hood_id)
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.hood = hood
+            post.user = request.user.profile
+            post.save()
+            return redirect('join_hood', hood.id)
+    else:
+        form = PostForm()
+    return render(request, 'post.html', {'form': form})
+
+@login_required(login_url='login')
+def add_business(request, hood_id):
+    hood = NeighbourHood.objects.get(id=hood_id)
+    if request.method == 'POST':
+        form = BusinessForm(request.POST)
+        if form.is_valid():
+            business = form.save(commit=False)
+            business.hood = hood
+            business.user = request.user.profile
+            business.save()
+            return redirect('join_hood', hood.id)
+    else:
+        form = BusinessForm()
+    return render(request, 'addbusiness.html', {'form': form})
+
+def search_business(request):
+    if 'business' in request.GET and request.GET["business"]:
+    # if request.method == 'GET':
+        search_term= request.GET.get("business")
+        results = Business.search_business(search_term)
+        print(results)
+        message = f'search_term'
+        context = {
+            'results': results,
+            'message': message
+        }
+        return render(request, 'search.html', context)
+    else:
+        message = "You haven't searched for any image category"
+    return render(request, 'search.html', {'message': message})
+
+
+
+
+
+
+
+
+
+
+
+
     
