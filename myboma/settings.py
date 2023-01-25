93628e4b52ef136b32fae96134749cd3ea043dfd
Dindihub <100135497+Dindihub@users.noreply.github.com>
2023-01-25 11:59:04 +0300

    update settings.py

--- myboma/settings.py
+++ myboma/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9h02k+m9_a5o_w*r9)f8y84k$(tsy_(2-db8jq5smca&$7_dvv'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
